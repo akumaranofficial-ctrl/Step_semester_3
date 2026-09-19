61dab3a4baac2280d7143cc3a710aa2b06fc3e51
sk8357-del <sk8357@srmist.edu.in>
2026-09-19 10:56:12 +0530

    q2

--- src/main/java/String4/class_problems/Solution.java
+++ src/main/java/String4/class_problems/Solution.java
@@ -1,7 +1,10 @@
 package String4.class_problems;
 
-class Solution {
-    public int[] twoSum(int[] nums, int target) {
+import java.util.Arrays;
+
+public class Solution {
+
+    public static int[] twoSum(int[] nums, int target) {
 
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
@@ -13,5 +16,15 @@ class Solution {
         }
 
         return new int[]{};
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+
+        int[] result = twoSum(nums, target);
+
+        System.out.println(Arrays.toString(result));
     }
 }
